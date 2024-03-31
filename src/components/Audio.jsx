4ca0532137ba5115f544292d3c9cd99f617b73e4
alejoforeroforero <alejoforeroforero@gmail.com>
2024-03-31 10:07:53 -0500

    animation frame inicio

--- src/components/Audio.jsx
+++ src/components/Audio.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { cambiarCancion, pararAudios } from '../Redux/states/managerSlice';
 import audioOnImg from '../assets/generales/audio-on.png';
@@ -6,12 +7,16 @@ import audioJorge1 from '../assets/guaviare/jorge/jorge1.mp3';
 import audioJorge2 from '../assets/guaviare/jorge/jorge2.mp3';
 import audioJorge3 from '../assets/guaviare/jorge/jorge3.mp3';
 import './Audio.css'
-import { useRef } from 'react';
+
 
 const Audio = ({ titulo, id }) => {
     const dispatch = useDispatch();
 
     const audioRef = useRef();
+    const canvasRef = useRef();
+    const frame = useRef(0);
+
+    const [animar, setAnimar] = useState(false);
 
     const cancionActual = useSelector(state => state.managerReducer.cancionActual);
 
@@ -27,6 +32,26 @@ const Audio = ({ titulo, id }) => {
         }
     }
 
+    const animate = () => {
+        //nextAnimationFrameHandler();
+        frame.current = requestAnimationFrame(animate);
+        console.log('holaaaaa');
+    };
+
+    useEffect(() => {
+        // start or continue animation in case of shouldAnimate if true
+        if (animar) {
+            frame.current = requestAnimationFrame(animate);
+            
+        } else {
+            // stop animation
+            cancelAnimationFrame(frame.current);
+        }
+
+        return () => cancelAnimationFrame(frame.current);
+    }, [animar]);
+
+
     const playAudio = () => {
 
         dispatch(pararAudios());
@@ -34,6 +59,7 @@ const Audio = ({ titulo, id }) => {
         if (cancionActual == id) {
             dispatch(cambiarCancion(null))
             audioRef.current.pause();
+            setAnimar(false)
         } else {
             dispatch(cambiarCancion(id))
 
@@ -41,6 +67,8 @@ const Audio = ({ titulo, id }) => {
 
             audioRef.current.src = path;
             audioRef.current.play();
+
+            setAnimar(true);
         }
     }
 
@@ -54,9 +82,11 @@ const Audio = ({ titulo, id }) => {
                 ref={audioRef}
                 src=""
                 controls
-                className={(audioS) ? 'mostrar' : 'esconder'}
+                // className={(audioS) ? 'mostrar' : 'esconder'}
+                className={(audioS) ? 'esconder' : 'esconder'}
             >
             </audio>
+            <canvas ref={canvasRef}></canvas>
         </div>
     )
 }
